b3cb9abcd982129311de123b12c8aa866d5f98d0
lqk <lqk@163.com>
2020-09-28 14:32:40 +0800

    123

---
+++ src/components/ABC.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <div v-for="i in abc">{{i}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  },
+  computed:{
+	  abc:function(){
+		var arr=[];
+		for(i=1;i<=10000;i++){
+			arr.push(i);
+		}
+		return arr;
+	  }
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
